a599b52a3d402109fe09986ba2183a2f3a2218a8
Victoria Hollingshead <103971332+vhollingshead@users.noreply.github.com>
2025-02-16 00:38:33 +0000

    added more subsubjects

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,39 @@ def about():
 def our_solution():
     st.title("Our Solution")
     st.write("Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.")
+    
+    st.subheader("High Level Overview")
+    st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+    
+    st.subheader("Methodology")
+    st.write("Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+    
+    st.subheader("Data pipeline")
+    st.write("Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.")
+    
+    st.subheader("Training dataset (if applicable)")
+    st.write("Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+    
+    st.subheader("Data pre-processing")
+    st.write("Curabitur pretium tincidunt lacus. Nulla gravida orci a odio.")
+    
+    st.subheader("Modelling")
+    st.write("Nullam varius, turpis et commodo pharetra, est eros bibendum elit.")
+    
+    st.subheader("Model Evaluation")
+    st.write("Donec odio tempus molestie, porttitor ut, iaculis quis, sem.")
+    
+    st.subheader("Key Learnings & Impact")
+    st.write("Maecenas libero. Curabitur suscipit suscipit tellus.")
+    
+    st.subheader("Key Contributions")
+    st.write("Sed lectus. Integer euismod lacus luctus magna.")
+    
+    st.subheader("Future Work")
+    st.write("Vivamus quis mi. Phasellus a est.")
+    
+    st.subheader("Acknowledgements")
+    st.write("Pellentesque dapibus hendrerit tortor. Praesent egestas tristique nibh.")
 
 def demo():
     st.title("Demo (MVP)")
