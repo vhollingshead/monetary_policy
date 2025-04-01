bf74d1589c1e1b75979dff89ab2bc8b06f1b2726
Victoria Hollingshead <103971332+vhollingshead@users.noreply.github.com>
2025-04-01 00:33:20 +0000

    update to time series plot

--- streamlit_app.py
+++ streamlit_app.py
@@ -315,6 +315,8 @@ def first_part():
             st.session_state.interest_rate = 5.0  # Reset to default
             st.session_state.m2_supply = 10.0  # Reset to default
             st.rerun()  # Ensures sliders reset
+        
+        st.markdown("<div class='white-box'>Adjust the Interest Rate Change & M2 Supply Change to see how the inequality forecast changes under different scenarios. Interest Rate Change is by percentage point, for example, a change of 2 percentage points will increase an original interest rate of 4% to a new interest rate of 6%. There are three main scenarios: tightening, neutral, and stimulus. In each scenario, we project the interest rate and M2 supply % change over a 5 year period.</div>", unsafe_allow_html=True)
 
         
         
